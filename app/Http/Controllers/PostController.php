1e9a154c91ecb6dedaf0964fc6cd4b767ab8b511
shu-36 <syu7222@gmail.com>
2021-03-29 15:03:52 +0900

    レバテックのカリキュラム２

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,9 +6,9 @@ use App\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
-{
+{  
     public function index(Post $post)
 {
-    return $post->get();
+    return view('index')->with(['posts' => $post->getPaginateByLimit()]);  
 }
 }
--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    
+ public function getPaginateByLimit(int $limit_count = 5)
+ {
+    return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
+    
+ }
 }
