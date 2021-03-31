6ff0322d181fd7d4868470e7644d443cb92d5ad9
shu-36 <syu7222@gmail.com>
2021-03-31 15:59:01 +0900

    マスターの直接変更

--- app/Post.php
+++ app/Post.php
@@ -1,15 +1,15 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App;
 
-use App/Post;
 use Illuminate\Database\Eloquent\Model;
 
-class PostController extends Controller
+class Post extends Model
 {
-    public function index(Post $post)
-    {
-        return view('index')->with(['posts' => $post->get()]);  
-    }
+    
+ public function getPaginateByLimit(int $limit_count = 5)
+ {
+    return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
+    
+ }
 }
-?>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,19 +3,21 @@
     <head>
         <meta charset="utf-8">
         <title>Blog</title>
-
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
-
-        
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
     <body>
-        <h1>Blog Nmae</h1>
-        <div class ='posts'>
-            <div class ='post'>
-                <h2 class ='title'>Title 1</h2>
-                <p class ='body'>This is a sample body.</p>
+       <h1>BlogName</h1>
+        <div class = 'posts'>
+            @foreach ($posts as $post)
+            <div class = 'post'>
+                <h2 class = 'title'>{{ $post->title }}</h2>
+                <p class = 'body'>{{ $post->body }}</p>
             </div>
+            @endforeach
         </div>
-    </body>
+         <div class ='paginate'>
+            {{ $posts->links() }}   
+         <div>
+     </body>
 </html>
