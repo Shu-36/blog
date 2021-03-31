8198d984c704a718e723fe4ffba43f721eab0d0b
shu-36 <syu7222@gmail.com>
2021-03-31 16:40:39 +0900

    お試し２

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,12 +1,12 @@
 <?php
-namespace App\Http\Controllers;
+ App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
 
-class PostController extends Controller
+class Controller extends BaseController
 {
-    public function index(Post $post)
-{
-    return $post->get();
-}
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
