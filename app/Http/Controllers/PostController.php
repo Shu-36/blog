3f07b786395f1710375b1d093a5f180365512db2
shu-36 <syu7222@gmail.com>
2021-03-31 15:46:22 +0900

    カリキュラム２の追加

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class PostController extends Controller
 {  
     public function index(Post $post)
-{
+ {
     return view('index')->with(['posts' => $post->getPaginateByLimit()]);  
-}
+ }
 }
