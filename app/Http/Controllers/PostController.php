80fe75f6588d6e4aa206f79d860df3941a549b7b
shu-36 <syu7222@gmail.com>
2021-04-05 15:36:37 +0900

    レバテックカリキュラム04

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,9 +1,12 @@
 <?php
 namespace App\Http\Controllers;
+
 use App\Post;
-use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest;
+
 class PostController extends Controller
 {  
+  //$Post=new post();のような感じ
     public function index(Post $post)
  {
     return view('index')->with(['posts' => $post->getPaginateByLimit()]);  
@@ -13,5 +16,17 @@ class PostController extends Controller
   {
       return view('show')->with(['post' => $post]);
   }
-
+  public function create()
+  {
+      return view('create');
+  }
+ public function store(Post $post, PostRequest $request)
+ {
+    //postの配列事態を取り出す
+     $input = $request['post'];
+     //postインスタンスにインサート構文を利用している？
+      $post->fill($input)->save();
+      //idを取得して記事詳細画面に遷移させる
+     return redirect('/posts/' . $post->id);
+ }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,6 +8,7 @@
     </head>
     <body>
        <h1>BlogName</h1>
+       <p class='create'>[<a href='/posts/create'>create</a>]</p>
         <div class = 'posts'>
             @foreach ($posts as $post)
             <div class = 'post'>
